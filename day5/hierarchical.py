b9478d601fcee808b468d01b12e2f8cb4511c199
Manyashree <manyashree.hj2005@gmail.com>
2026-02-07 12:39:50 +0530

    some changes made in code

--- day5/hierarchical.py
+++ day5/hierarchical.py
@@ -5,42 +5,56 @@ class BankAccount:
 
     def deposit(self,amount):
         self.balance+=amount
-        print(f"deposited {amount}")
+        print(f"deposited {amount},balance is {self.balance}")
 
     def withdraw(self,amount):
         if amount<=self.balance:
             self.balance-=amount
-            print(f"withdraw {amount}")
+            print(f"withdraw {amount},balance is {self.balance}")
         else:
             print("insufficient balance")
 
-    def display_balance(self,amount):
-        print("current balance {self.balance}")
+    def display_balance(self):
+        print(f"current balance: {self.balance}")
 
 class SavingAccount(BankAccount):
-    def __init__(self,interest_rate,account_holder,balance):
+    def __init__(self,interest_rate,account_holder):
         self.interest_rate=interest_rate
-        super().__init__(account_holder,balance)
+        super().__init__(account_holder)
     
     def add_interest(self):
-        interest=(sself.balance*interest_rate)/100
+        interest=(self.balance*self.interest_rate)/100
         self.balance+=interest
-        print("the interest is:",interest)
+        print(f"the interest is {interest}, the balance is {self.balance}")
 
 class CurrentAccount(BankAccount):
-    def __init__(self,overdraft_limit,account_holder,balance):
+    def __init__(self,overdraft_limit,account_holder):
         self.overdraft_limit=overdraft_limit
-        super().__init__(account_holder,balance)
+        super().__init__(account_holder)
 
     def withdraw_with_overdraft(self,amount):
         if amount <= self.balance + self.overdraft_limit:
             self.balance -= amount
-            print(f"Withdrawn using overdraft: ₹{amount}")
+            print(f"Withdrawn using overdraft: ₹{amount},balance is {self.balance}")
         else:
             print("Overdraft limit exceeded")
 
-s = SavingsAccount("Anita", 5000, 5)
-s.deposit(1000)
-s.add_interest()
-s.withdraw(2000)
-s.display_balance()
+bank3=CurrentAccount(200,"manya")
+bank3.deposit(1000)
+bank3.withdraw_with_overdraft(1200)
+
+# bank1=BankAccount("manya")
+# bank1.deposit(1000)
+# bank1.withdraw(1000)
+# bank1.display_balance()
+
+# bank2=BankAccount("manya")
+# bank2.deposit(1000)
+# bank2.withdraw(500)
+
+# bank2=SavingAccount(10,"manya")
+# bank2.deposit(1000)
+# bank2.withdraw(500)
+# bank2.add_interest()
+# bank2.deposit(1000)
+# bank2.display_balance()
--- day5/hybrid.py
+++ day5/hybrid.py
@@ -8,23 +8,79 @@ class Student(Person):
         self.student_id=student_id
         Person.__init__(self,name)
     def display_student(self):
-        print("student id",self.student_id)
+        print("student id:",self.student_id)
+
 class SportsPlayer(Person):
     def __init__(self,sport_name,name):
         self.sport_name=sport_name
         Person.__init__(self,name)
     def display_sports_player(self):
-        print("sports_player is",self.sport_name)
+        print("sports is",self.sport_name)
+
 class CollegeStudent(Student,SportsPlayer):
     def __init__(self,college_name,student_id,sport_name,name):
         self.college_name=college_name
         Student.__init__(self,student_id,name)
         SportsPlayer.__init__(self,sport_name,name)
     def diplay_college_student(self):
-        print(self.college_name)
+        print("college name:",self.college_name)
 
 obj1=CollegeStudent("mitt","cs005","chess","manya")
 obj1.diplay_college_student()
 obj1.display_sports_player()
 obj1.display_student()
 obj1.display_person()
+
+obj2=SportsPlayer("chess","manya")
+obj2.display_sports_player()
+obj2.display_person()
+
+obj3=Student("chess","manya")
+obj3.display_student()
+obj3.display_person()
+
+
+"""
+#works in cs,sp but error in s
+class Person:
+    def __init__(self,name):
+        self.name=name
+    def display_person(self):
+        print("the name is:",self.name)
+class Student(Person):
+    def __init__(self,student_id,name):
+        self.student_id=student_id
+        #Person.__init__(self,name)
+        super().__init__(self,name)
+    def display_student(self):
+        print("student id:",self.student_id)
+
+class SportsPlayer(Person):
+    def __init__(self,sport_name,name):
+        self.sport_name=sport_name
+        # Person.__init__(self,name)
+        super().__init__(name)
+    def display_sports_player(self):
+        print("sports is",self.sport_name)
+
+class CollegeStudent(Student,SportsPlayer):
+    def __init__(self,college_name,student_id,sport_name,name):
+        self.college_name=college_name
+        Student.__init__(self,student_id,name)
+        SportsPlayer.__init__(self,sport_name,name)
+    def diplay_college_student(self):
+        print("college name:",self.college_name)
+
+obj1=CollegeStudent("mitt","cs005","chess","manya")
+obj1.diplay_college_student()
+obj1.display_sports_player()
+obj1.display_student()
+obj1.display_person()
+
+obj2=SportsPlayer("chess","manya")
+obj2.display_sports_player()
+obj2.display_person()
+
+obj3=Student`("chess","manya")
+obj3.display_person()
+"""
