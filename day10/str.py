6f6ac10d2527c189834fba008011c065b21be59b
Manyashree <manyashree.hj2005@gmail.com>
2026-02-12 07:39:50 +0530

    dunder method

---
+++ day10/str.py
@@ -0,0 +1,11 @@
+class Bokk:
+    def __init__(self,title,author,price):
+        self.title=title
+        self.author=author
+        self.price=price
+    def _str__(self):
+        return f"Book:{self.title} by {self.author},price:{self.price}"
+    def __repr__:
+        
+
+    
